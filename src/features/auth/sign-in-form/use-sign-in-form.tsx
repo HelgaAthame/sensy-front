17e1383f067279e40deea12a76a261c7f131b633
Marchuk98 <vladimir.fmr98@gmail.com>
2025-04-01 13:13:37 +0300

    SNS-4 feat: adding a login form with validation

---
+++ src/features/auth/sign-in-form/use-sign-in-form.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]).*$/
+
+export const useSignInForm = () => {
+  const loginFormSchema = z.object({
+    email: z
+      .string()
+      .trim()
+      .min(1, 'Введите адрес электронной почты')
+      .email('Некорректный адрес электронной почты'),
+    password: z
+      .string()
+      .trim()
+      .min(1, 'Введите пароль')
+      .min(8, 'Пароль должен содержать как минимум 8 символов')
+      .max(30, 'Пароль должен содержать не более 30 символов')
+      .regex(
+        passwordRegex,
+        'Пароль должен содержать как минимум одну строчную букву (a-z), одну цифру (0-9) и один специальный символ (например, @, #, $ и т.п.).'
+      ),
+    rememberMe: z.boolean().optional(),
+  })
+
+  type LoginFormType = z.infer<typeof loginFormSchema>
+
+  return useForm<LoginFormType>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onBlur',
+    resolver: zodResolver(loginFormSchema),
+  })
+}
