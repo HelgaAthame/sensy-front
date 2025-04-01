7bb174f0b55e6ef90f3b510a0433f9b92aa29b5c
Marchuk98 <vladimir.fmr98@gmail.com>
2025-04-01 13:03:59 +0300

    SNS-3 feat: setting variables, colors, fonts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,36 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            typescript: true,
+            dimensions: false,
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'preset-default',
+                  params: {
+                    overrides: {
+                      removeViewBox: false
+                    }
+                  }
+                }
+              ]
+            }
+          }
+        }
+      ]
+    });
+
+    return config;
+  }
 };
 
 export default nextConfig;
