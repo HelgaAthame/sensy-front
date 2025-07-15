feeefacb38bdc3ef3eacac1cad1a177a873f281c
olga <khmarukolgaivanovna@gmail.com>
2025-07-15 10:46:45 +0300

    feat: create checklists table

--- src/features/checklists/checklists.tsx
+++ src/features/checklists/checklists.tsx
@@ -1,40 +1,42 @@
 'use client';
 
-import ComponentCard from '@/shared/ui/component-card/component-card';
-// import { ChecklistsTable } from '@/features/checklists/checklists-table/checklists-table'
-// import { useGetChecklistsQuery } from '@/entities/checklists/checklists.api'
+import { ChecklistsTable } from '@/features/checklists/checklists-table/checklists-table';
+import { useGetChecklistsQuery } from '@/entities/checklists/checklists.api';
 import { LoaderContent } from '@/shared/ui/loader';
 
 export const Checklists = () => {
-  // const { data: operatorsData, isLoading } = useGetChecklistsQuery()
+  const { data: checklistsData, isLoading } = useGetChecklistsQuery();
 
-  // if (isLoading) {
-  //   return <div>Загрузка...</div>
-  // }
+  if (isLoading) {
+    return <div>Загрузка...</div>;
+  }
 
-  const columns = [{ key: 'name', title: 'Название проекта' }];
+  const columns = [
+    { key: 'name', title: 'Чек-листы' },
+    { key: 'isActive', title: 'Опубликован' },
+  ];
 
   return (
     <>
-      {/* {isLoading ? (
+      {isLoading ? (
         <LoaderContent width={200} height={200} isLoading={isLoading} />
       ) : (
-        <>*/}
+        <>
           <h2
             className="text-xl font-semibold text-gray-800 dark:text-white/90 mb-6"
             x-text="pageName"
           >
             Чек-листы
           </h2>
-          <div className="space-y-6">
-            {/* <ComponentCard>
-              {operatorsData && (
-                <ChecklistsTable data={operatorsData} columns={columns} itemsPerPage={10} />
-              )}
-            </ComponentCard> */}
-          </div>
-       {/* </>
-      )} */}
+          {checklistsData && (
+            <ChecklistsTable
+              data={checklistsData}
+              columns={columns}
+              itemsPerPage={10}
+            />
+          )}
+        </>
+      )}
     </>
   );
 };
